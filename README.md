3b7c0e802271fc8053c94ab51e279e789dd39f84
Jason <jasontwuk@gmail.com>
2020-11-08 12:42:28 +0000

    add sun and moon feature

---
+++ README.md
@@ -0,0 +1,4 @@
+# JavaScript-Project_Analog-Clock
+This is an execise originally from <a href="https://github.com/WebDevSimplified/JavaScript-Clock">WebDevSimplified</a>.
+
+Live site: https://jasontwuk.github.io/JavaScript-Project_Analog-Clock/
--- script.js
+++ script.js
@@ -3,18 +3,38 @@ const secondHand = document.getElementById("second");
 const minuteHand = document.getElementById("minute");
 const hourHand = document.getElementById("hour");
 
+const body = document.body;
+const clock = document.getElementById("clock");
+const sun = document.getElementById("sun");
+const moon = document.getElementById("moon");
+
 // ! set function
 setInterval(setClock, 1000);
 
 function setClock() {
-  let date = new Date();
+  const date = new Date();
+  // date.setHours(5);
   // console.log(date);
-  let secondRatio = date.getSeconds() / 60;
-  let minuteRatio = (date.getMinutes() + secondRatio) / 60;
-  let hourRatio = (date.getHours() + minuteRatio) / 12;
+  const hours = date.getHours();
+  // *** get 'true', when the hour is between 6:00 and 18:00
+  const isDayTime = hours > 5 && hours < 18;
+
+  const secondRatio = date.getSeconds() / 60;
+  const minuteRatio = (date.getMinutes() + secondRatio) / 60;
+  const hourRatio = (date.getHours() + minuteRatio) / 12;
   secondHand.style.transform = "rotate(" + secondRatio * 360 + "deg)";
   minuteHand.style.transform = "rotate(" + minuteRatio * 360 + "deg)";
   hourHand.style.transform = "rotate(" + hourRatio * 360 + "deg)";
+
+  displaySunOrMoon(isDayTime);
+}
+
+function displaySunOrMoon(condition) {
+  if (condition) {
+    body.classList.remove("night-mode");
+  } else {
+    body.classList.add("night-mode");
+  }
 }
 
 setClock();
